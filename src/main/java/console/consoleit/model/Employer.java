7ce247c3f7c1afa6b934e913c5a5905f89023a32
UIshfaq <ishfaqumair93380@gmail.com>
2025-01-07 15:34:08 +0100

    employer

--- src/main/java/console/consoleit/model/Employer.java
+++ src/main/java/console/consoleit/model/Employer.java
@@ -1,17 +1,27 @@
 package console.consoleit.model;
 
-import java.util.Date;
+import java.sql.Date;
 
 public class Employer {
     private int id;
     private String nom;
     private String prenom;
+    private String ville;
+    private String codePostal;
+    private String adresse;
+    private java.sql.Date dateNais;
+    private String telephone;
 
 
-    public Employer(int id, String nom, String prenom) {
+    public Employer(int id, String nom, String prenom, String ville, String codePostal, String adresse, Date dateNais, String telephone) {
         this.id = id;
         this.nom = nom;
         this.prenom = prenom;
+        this.ville = ville;
+        this.codePostal = codePostal;
+        this.adresse = adresse;
+        this.dateNais = dateNais;
+        this.telephone = telephone;
     }
 
     public int getId() {
@@ -36,5 +46,45 @@ public class Employer {
 
     public void setPrenom(String prenom) {
         this.prenom = prenom;
+    }
+
+    public String getVille() {
+        return ville;
+    }
+
+    public void setVille(String ville) {
+        this.ville = ville;
+    }
+
+    public String getCodePostal() {
+        return codePostal;
+    }
+
+    public void setCodePostal(String codePostal) {
+        this.codePostal = codePostal;
+    }
+
+    public String getAdresse() {
+        return adresse;
+    }
+
+    public void setAdresse(String adresse) {
+        this.adresse = adresse;
+    }
+
+    public Date getDateNais() {
+        return dateNais;
+    }
+
+    public void setDateNais(Date dateNais) {
+        this.dateNais = dateNais;
+    }
+
+    public String getTelephone() {
+        return telephone;
+    }
+
+    public void setTelephone(String telephone) {
+        this.telephone = telephone;
     }
 }
