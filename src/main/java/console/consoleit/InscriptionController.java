51795ad5913e94875182d51c76761a6dbf0849c8
UIshfaq <ishfaqumair93380@gmail.com>
2025-01-07 14:36:01 +0100

    Incription #3

---
+++ src/main/java/console/consoleit/InscriptionController.java
@@ -0,0 +1,23 @@
+package console.consoleit;
+
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
+public class InscriptionController {
+    @javafx.fxml.FXML
+    private TextField txtVille;
+    @javafx.fxml.FXML
+    private TextField txtPrenom;
+    @javafx.fxml.FXML
+    private TextField txtCodePos;
+    @javafx.fxml.FXML
+    private TextField txtTel;
+    @javafx.fxml.FXML
+    private TextField txtMdp;
+    @javafx.fxml.FXML
+    private TextField txtAdresse;
+    @javafx.fxml.FXML
+    private Button btnValider;
+    @javafx.fxml.FXML
+    private TextField txtNom;
+}
--- src/main/java/console/consoleit/model/Mission.java
+++ src/main/java/console/consoleit/model/Mission.java
@@ -1,4 +1,29 @@
 package console.consoleit.model;
 
 public class Mission {
+    private int id;
+    private String nomMission;
+
+
+    public Mission(int id, String nomMission) {
+        this.id = id;
+        this.nomMission = nomMission;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNomMission() {
+        return nomMission;
+    }
+
+    public void setNomMission(String nomMission) {
+        this.nomMission = nomMission;
+    }
+
 }
