7ee83cee414206669e03b95857ece6b7292f3f21
UIshfaq <ishfaqumair93380@gmail.com>
2025-01-10 14:34:17 +0100

    tableau de bord #4

--- src/main/java/console/consoleit/AdminTableauBordController.java
+++ src/main/java/console/consoleit/AdminTableauBordController.java
@@ -4,10 +4,13 @@ import console.consoleit.controllers.AdminController;
 import console.consoleit.model.Employer;
 import console.consoleit.tools.DataSourceProvider;
 import javafx.collections.FXCollections;
+import javafx.event.Event;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
 import javafx.scene.control.cell.PropertyValueFactory;
 
 import java.net.URL;
@@ -33,13 +36,23 @@ public class AdminTableauBordController implements Initializable {
     @FXML
     private TableColumn tcSite;
     @FXML
-    private TableColumn<Employer,Boolean> tcBloque;
-    @FXML
     private TableColumn tcEmail;
     @FXML
     private TableColumn tcNomMission;
     @FXML
     private TableView tvMission;
+    @FXML
+    private Button brnSupp;
+    @FXML
+    private Button btnCreer;
+    @FXML
+    private TextField txtMateriel;
+    @FXML
+    private Button btnModif;
+    @FXML
+    private TextField txtSite;
+    @FXML
+    private TextField txtNomMission;
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
@@ -58,4 +71,15 @@ public class AdminTableauBordController implements Initializable {
         }
     }
 
+    @FXML
+    public void btnModifCliked(Event event) {
+    }
+
+    @FXML
+    public void btnCreerCliked(Event event) {
+    }
+
+    @FXML
+    public void BtnSuppCliked(Event event) {
+    }
 }
